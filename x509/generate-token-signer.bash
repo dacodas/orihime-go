158d77f290d48e8403c51200f3e9d457b445a37b
Dacoda Strack <dacoda@fedora.omen.home>
2020-03-10 23:01:28 -0700

    add PSS options to token signer

--- x509/generate-token-signer.bash
+++ x509/generate-token-signer.bash
@@ -8,7 +8,11 @@ PRIVATE_KEY=token-signer.private.pem
 PUBLIC_KEY=token-signer.public.pem
 
 FILE_TO_SIGN="$1"
-SIGNATURE_OUTPUT="$2"
+DIGEST_OUTPUT="$2"
+SIGNATURE_OUTPUT="$3"
+
+PKEYUTL_OPTIONS="-pkeyopt digest:sha256 -pkeyopt rsa_padding_mode:pss -inkey ${PRIVATE_KEY} "
+PKEYUTL_SIGN_OPTIONS="-pkeyopt rsa_pss_saltlen:256 -pkeyopt rsa_mgf1_md:sha256"
 
 if [ "x${GENERATE_NEW_KEY}" = "xtrue" ]
 then
@@ -16,5 +20,7 @@ then
 	openssl rsa -outform PEM -in ${PRIVATE_KEY} -pubout -out ${PUBLIC_KEY}
 fi
 
-openssl dgst -sha256 -sign ${PRIVATE_KEY} -out ${SIGNATURE_OUTPUT} ${FILE_TO_SIGN}
-openssl dgst -sha256 -verify ${PUBLIC_KEY} -signature ${SIGNATURE_OUTPUT} ${FILE_TO_SIGN}
+openssl dgst -sha256 -binary -out ${DIGEST_OUTPUT} ${FILE_TO_SIGN}
+openssl pkeyutl ${PKEYUTL_OPTIONS} ${PKEYUTL_SIGN_OPTIONS} -in ${DIGEST_OUTPUT} -sign -out ${SIGNATURE_OUTPUT}
+openssl pkeyutl ${PKEYUTL_OPTIONS} -in ${DIGEST_OUTPUT} -verify -sigfile ${SIGNATURE_OUTPUT}
+# openssl dgst -sha256 -verify ${PUBLIC_KEY} -signature ${SIGNATURE_OUTPUT} ${FILE_TO_SIGN}
