6c027355d3096a0832c10b1411a08b250696aaf0
Dacoda Strack <dacoda.strack@gmail.com>
2020-03-12 15:59:03 -0700

    check signed token before commiting to database

--- internal/server/server.go
+++ internal/server/server.go
@@ -15,21 +15,33 @@ func NewServer() *OrihimeServer {
 }
 
 func (s *OrihimeServer) AddText(ctx context.Context, req *protobuf.TextToAdd) (*protobuf.TextAdded, error) {
-	database.AddText(req.Content, req.Source)
+	verified := VerifyCallToken(ctx)
+	if verified {
+		database.AddText(req.Content, req.Source)
+	}
 	return &protobuf.TextAdded{}, nil
 }
 
 func (s *OrihimeServer) AddWord(ctx context.Context, req *protobuf.WordToAdd) (*protobuf.WordAdded, error) {
-	database.AddWord(req.Word, req.DefinitionText, req.Source)
+	verified := VerifyCallToken(ctx)
+	if verified {
+		database.AddWord(req.Word, req.DefinitionText, req.Source)
+	}
 	return &protobuf.WordAdded{}, nil
 }
 
 func (s *OrihimeServer) AddSource(ctx context.Context, req *protobuf.SourceToAdd) (*protobuf.SourceAdded, error) {
-	database.AddSource(req.Source)
+	verified := VerifyCallToken(ctx)
+	if verified {
+		database.AddSource(req.Source)
+	}
 	return &protobuf.SourceAdded{}, nil
 }
 
 func (s *OrihimeServer) AddChildWord(ctx context.Context, req *protobuf.ChildWordToAdd) (*protobuf.ChildWordAdded, error) {
-	database.AddChildWord(req.Word, req.Definition, req.Source, req.User, req.ParentTextHash)
+	verified := VerifyCallToken(ctx)
+	if verified {
+		database.AddChildWord(req.Word, req.Definition, req.Source, req.User, req.ParentTextHash)
+	}
 	return &protobuf.ChildWordAdded{}, nil
 }
