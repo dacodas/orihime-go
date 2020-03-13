eb58598bf4a600e954922d03b2830fe90cc78e47
Dacoda Strack <dacoda.strack@gmail.com>
2020-03-13 14:29:27 -0700

    add cursory populate tool

---
+++ tools/populate/populate.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+NUMBER="$1"
+
+TEXT="Here is a text from some source ${NUMBER}"
+TEXT_SOURCE="Some source"
+WORD="source ${NUMBER}"
+DEFINITION="Place to get stuff from ${NUMBER}"
+DEFINITION_SOURCE="Some dictionary"
+
+# orihime add source "${TEXT_SOURCE}"
+# orihime add source "${DEFINITION_SOURCE}"
+
+orihime add text "${TEXT}" --source "${TEXT_SOURCE}"
+orihime add child-word "${WORD}" --user 'dacoda.strack@gmail.com' --source "${DEFINITION_SOURCE}" --definition "${DEFINITION}" --parent-text $(echo -n "${TEXT}" | sha256sum | sed 's/  -//')
